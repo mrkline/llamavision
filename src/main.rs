ed80778d6db49e18133752e9b456263b467ea820
Matt Kline <matt@bitbashing.io>
2024-06-30 23:55:35 -0700

    Buffer a few audio chunks

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ fn run(args: Args) -> Result<()> {
     let height = args.height.unwrap_or(512);
 
     let (err_tx, err_rx) = channel();
-    let (audio_tx, audio_rx) = sync_channel(0);
+    let (audio_tx, audio_rx) = sync_channel(8);
     let (rows_tx, rows_rx) = sync_channel(0);
     fanout(&err_tx, "fft", move || fft::run(width, &FFTW_READY, audio_rx, rows_tx));
     fanout(&err_tx, "pipewire", move || pipewire::run(&FFTW_READY, audio_tx));
